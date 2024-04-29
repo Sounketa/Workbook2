19430b6ffd7190698ed17a0e66c71d119fdedd6e
Sounketa <julien.ilboudo97@gmail.com>
2024-04-29 14:23:48 -0400

    update7

--- demos/example2/calculator.js
+++ demos/example2/calculator.js
@@ -1,18 +1,66 @@
 "use strict";
 
-function add() {
-    var pageOne = 150;
-    var pageTwo = 200;
+// function add() {
+//     var numberOne = 83;
+//     var numberTwo = 25;
+    
+//     var result = numberOne + numberTwo;
+    
+//     console.log(result)
+    
+// }
+// // add();
 
-    var combine = pageOne + pageTwo;
+//  // .getElementById reutrns "null" if it cannot find id, if it did it would return an "html element"
+// function init() {
+//     var addButton = document.getElementById("addButton")  
+//     addButton.onclick = add;
+// }
+// // init();
+// window.onload = init;
+// // button.onclick = add
 
-    console.log(combine);
+function addButtonClick() {
+    var number1 =  Number(document.getElementById("numberOneInput").value);
+    var number2 =  Number(document.getElementById("numberTwoInput").value);
+    var result = number1 + number2;
+    document.getElementById("answer").value = result;
 }
-// add();
 
-function init() {
-    console.log("hello")
+function subtractButtonClick() {
+    var number1 =  Number(document.getElementById("numberOneInput").value);
+    var number2 =  Number(document.getElementById("numberTwoInput").value);
+    var result = number1 - number2;
+    document.getElementById("answer").value = result;
 }
-// init();
-window.onload = init;
-button.onclick = add
+
+function multiplyButtonClick() {
+    var number1 =  Number(document.getElementById("numberOneInput").value);
+    var number2 =  Number(document.getElementById("numberTwoInput").value);
+    var result = number1 * number2;
+    document.getElementById("answer").value = result;
+}
+
+function divideButtonClick() {
+    var number1 =  Number(document.getElementById("numberOneInput").value);
+    var number2 =  Number(document.getElementById("numberTwoInput").value);
+    var result = number1 / number2;
+    document.getElementById("answer").value = result;
+}
+
+
+window.onload = function init(){
+    var addButtonOne = document.getElementById("addButton")
+    addButtonOne.onclick = addButtonClick;
+
+    var subtractButtonOne = document.getElementById("subtractButton")
+    subtractButtonOne.onclick = subtractButtonClick;
+
+    
+    var multiplyButtonOne = document.getElementById("multiplyButton")
+    multiplyButtonOne.onclick = multiplyButtonClick;
+
+    
+    var divideButtonOne = document.getElementById("divideButton")
+    divideButtonOne.onclick = divideButtonClick;
+}
