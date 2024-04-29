a805c66514bac39d4bec92e82879e66004fc2d18
Sounketa <julien.ilboudo97@gmail.com>
2024-04-29 10:23:03 -0400

    update5

---
+++ demos/example1/index.js
@@ -0,0 +1,22 @@
+// "use strict";
+
+// Concept One, would be better to use "let" instead of  "var".
+var numberOne = 83;
+var numberTwo = 25;
+
+var result = numberOne + numberTwo;
+
+console.log(result)
+
+// Concept Two; function then function"name" - parent and child relationship, call function
+function add() {
+    var pageOne = 150;
+    var pageTwo = 200;
+
+    var combine = pageOne + pageTwo;
+
+    console.log(combine);
+}
+(add)
+// This calla the function, like answering your parent after they called you
+
